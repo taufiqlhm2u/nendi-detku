ddc8e5a0a06c2d74edde76f332b3cd975eed376f
taufiq <cellijo724@gmail.com>
2026-06-18 19:43:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,32 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\BerandaController;
+use App\Http\Controllers\User\IncomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('login.verif');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store'])->name('login.verif');
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store'])->name('login.verif');
 
-Route::middleware('auth')->group(function() {
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+});
+
+Route::middleware('auth')->group(function () {
     Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda');
 
+    Route::resource('pemasukan', IncomeController::class)->names('incomes');
 
 
     Route::post('/logout', [LoginController::class, 'logout'])
-    ->name('logout');
+        ->name('logout');
 });
