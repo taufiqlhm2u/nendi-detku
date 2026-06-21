8e12ca8515dc477a0d0068fa093cae506c54c484
taufiq <cellijo724@gmail.com>
2026-06-21 20:49:49 +0700

    detail

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,6 +1,9 @@
 <x-layout>
     <div class="bg-[#f9f9ff] text-[#191c21] min-h-screen pb-20">
 
+        {{-- ===== Top App Bar ===== --}}
+        <x-user-header />
+
         <livewire:user.beranda />
 
         <!-- ===== Floating Action Button ===== -->
